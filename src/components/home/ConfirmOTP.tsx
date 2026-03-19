9d903fd02de6402748c89e3cb8c11084909b6242
Gokul Gopi <gokulg1412@gmail.com>
2026-03-19 23:59:55 +0530

    feat: resend otp logic

--- src/components/home/ConfirmOTP.tsx
+++ src/components/home/ConfirmOTP.tsx
@@ -5,8 +5,15 @@ import FormButtons from "./FormButtons";
 import { otpSchema, TOTPSchema } from "@/utils/validationSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@/components/ui/Button";
+import { useEffect, useRef, useState } from "react";
+import { formatTime } from "@/utils/helpers";
+
+const RESEND_OTP_TIME = 60;
 
 const ConfirmOTP = () => {
+  const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+  const [resendOtpTime, setResendOtpTime] = useState(RESEND_OTP_TIME);
+
   const form = useForm<TOTPSchema>({
     resolver: zodResolver(otpSchema),
     defaultValues: {
@@ -14,27 +21,52 @@ const ConfirmOTP = () => {
     },
   });
 
+  const formattedTime = formatTime(resendOtpTime);
+
   const onSubmit = form.handleSubmit((data) => {
     console.log({ data });
   });
+
+  const onResendOtp = () => {
+    setResendOtpTime(RESEND_OTP_TIME);
+  };
+
+  useEffect(() => {
+    if (resendOtpTime <= 0) return;
+
+    timeoutRef.current = setTimeout(() => {
+      setResendOtpTime(resendOtpTime - 1);
+    }, 1000);
+
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
+  }, [resendOtpTime]);
 
   return (
     <FormProvider {...form}>
       <form onSubmit={onSubmit} className="flex flex-1 flex-col">
         <FormHeader title="OTP Verification" />
 
-        <div className="flex flex-col items-center gap-4 lg:w-fit">
+        <div className="flex flex-col gap-4 max-lg:items-center lg:w-fit">
           <OTPInput
             label="An OTP has been sent to your mobile number"
             required
             name="otp"
-            className="items-center"
+            className="max-lg:items-center"
           />
 
           <div className="flex items-center gap-1 lg:ml-auto">
             <p className="text-sm">Didn't receive the OTP?</p>
-            <Button variant="link" className="px-0">
-              Resend OTP
+            <Button
+              variant="link"
+              className="px-0"
+              disabled={resendOtpTime > 0}
+              onClick={onResendOtp}
+            >
+              Resend OTP {resendOtpTime > 0 ? `in ${formattedTime}s` : null}
             </Button>
           </div>
         </div>
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -28,3 +28,9 @@ export const COUNTRY_CODES = [
     length: 10,
   },
 ] as const;
+
+export const formatTime = (seconds: number) => {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  return `${minutes.toString().padStart(2, "0")}:${remainingSeconds.toString().padStart(2, "0")}`;
+};
